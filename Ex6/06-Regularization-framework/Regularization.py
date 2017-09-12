6e2ba723e0fbeb36c656c36fc0d59edb00f3483a
zeitunik <osipovakarine@gmail.com>
2017-09-12 22:15:03 +0200

    final tweaks

--- Ex6/06-Regularization-framework/Regularization.py
+++ Ex6/06-Regularization-framework/Regularization.py
@@ -20,12 +20,12 @@ def gkern(size=3, sigma=1.):
     kernel = np.exp(-(x**2 + y**2) / (2. * sigma**2))
     return kernel / np.sum(kernel)
 
-l = 0.001
-rho = 0.5 # strength of regularization
+l = 0.0009
+rho = 0.9 # strength of regularization
 sigma = 0.01 # the amount of noise
-iterations = 10
+iterations = 100
 
-kernel = gkern(size=9, sigma=3)
+kernel = gkern(size=9, sigma=1)
 # kernel = np.ones((3,3))
 # print(C)
 d_x = np.array([[0,0,0],[0,-1,1],[0,0,0]])
@@ -38,24 +38,15 @@ def C(x):
 	return blur.flatten()
 
 def D(x):
-	# ---------
-	# your code
-	# ---------
 	img = x.reshape(imgShape)
 	delta_x = convolve(img, d_x, 'same')
 	delta_y = convolve(img, d_y, 'same')
 	return np.hstack([delta_x.flatten(), delta_y.flatten()])
 
 def MatVec(x):
-	# ---------
-	# your code
-	# ---------
 	return np.hstack([C(x), rho*D(x)])
 	
 def RMatVec(x):
-	# ---------
-	# your code
-	# ---------
 	y1 = x[:pix].reshape(imgShape)
 	y2 = x[pix:2*pix].reshape(imgShape)
 	y3 = x[2*pix:3*pix].reshape(imgShape)
@@ -68,9 +59,6 @@ def RMatVec(x):
 
 
 def shrink(a, k):
-	# ---------
-	# your code
-	# ---------
 	ret = np.zeros(a.shape)
 	ret[a>k] = a[a>k] - k
 	ret[a<-k] = a[a<-k] + k
@@ -90,25 +78,22 @@ def Optimization(b):
 		print(i)
 		
 		# x step:
-		# ...
 		v = np.hstack([b, rho*(z-u)])
 		x = lsmr(A, v)[0] #show=True, atol= , btol =
 		
 		# z step
-		# ...
 		w = D(x) + u
 		z = shrink(w, l/rho)
 
 		# u step
-		# ...
 		u = w - z
-		
-		
+
 		print("Error: ", np.linalg.norm(img.flatten()-x))
 	
 	# Plot the results
-	# ...
-	plt.imshow(x.reshape(imgShape))
+	
+	fig = plt.imshow(x.reshape(imgShape)).figure
+	fig.suptitle("lambda={}, rho={}".format(l, rho))
 	plt.show()
 	
 	return x.reshape(imgShape)
@@ -116,10 +101,6 @@ def Optimization(b):
 	
 
 def EvaluateLinOp():
-	# ---------
-	# your code
-	# ---------
-
 	f = np.ones(pix)
 	h = np.ones(3*pix)
 	rhs1 = MatVec(f)
@@ -128,12 +109,9 @@ def EvaluateLinOp():
 	val1 = rhs1.dot(h)
 	val2 = rhs2.dot(f)
 	print("val1 = {}\nval2 = {}".format(val1, val2))
-	assert (val1-val2 <= 1.e-5)
+	assert (abs(val1-val2) <= 1.e-6)
 	
 def EvaluateShrink():
-	# ---------
-	# your code
-	# ---------
 	x = np.arange(-10, 11)
 	s = shrink(x, 3)
 	plt.plot(x, s)
@@ -146,12 +124,12 @@ def EvaluateShrink():
 	
 # evaluation of tools
 EvaluateLinOp()
-EvaluateShrink()
+# EvaluateShrink()
 
 # # deblurring
-# plt.imsave("original.png", img)
+plt.imsave("original.png", img)
 blurred = convolve(img, kernel, 'same')
 blurred += np.random.normal(0, sigma, blurred.shape)
-# plt.imsave("blurred.png", blurred)
+plt.imsave("blurred.png", blurred)
 x = Optimization(blurred)
-# plt.imsave("result.png", x)
+plt.imsave("result.png", x)

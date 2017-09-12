4bc25ad01f99b35e55bda9e7eae2c8013c72e4d0
zeitunik <osipovakarine@gmail.com>
2017-09-12 20:35:41 +0200

    -Implemented convolution and correlation functions and test

--- Ex6/06-Regularization-framework/Regularization.py
+++ Ex6/06-Regularization-framework/Regularization.py
@@ -8,11 +8,9 @@ from scipy.sparse.linalg import lsmr
 plt.close('all')
 
 
-
-
 # load image, crop it and take the red channel:
 img = plt.imread("Lenna.png")[200:300,200:320,0]
-#img = plt.imread("Lenna.png")[250:280,250:290,0]
+# img = plt.imread("Lenna.png")[250:254,250:254,0]
 imgShape = img.shape
 pix = imgShape[0]*imgShape[1] # number of pixels
 
@@ -24,31 +22,59 @@ pix = imgShape[0]*imgShape[1] # number of pixels
 # d_y = ...
 # ---------
 
+def gkern(size=3, sigma=1.):
+    ax = np.arange(-size // 2 + 1., size // 2 + 1.)
+    x, y = np.meshgrid(ax, ax)
+    kernel = np.exp(-(x**2 + y**2) / (2. * sigma**2))
+    return kernel / np.sum(kernel)
 
 l = 0.001
 rho = 0.1 # strength of regularization
 sigma = 0.01 # the amount of noise
 iterations = 10
 
+kernel = gkern(size=3, sigma=1)
+# kernel = np.ones((3,3))
+# print(C)
+d_x = np.array([[0,0,0],[0,-1,1],[0,0,0]])
+d_y = np.array([[0,0,0],[0,-1,0],[0,1,0]])
+
+
+def C(x):
+	# assume x given in imgShape
+	# kernel = gkern(size=9, sigma=1)
+	blur = convolve(x, kernel, 'same')
+	return blur.flatten()
 
 def D(x):
 	# ---------
 	# your code
 	# ---------
-	pass
-
+	# assume x given in imgShape
+	delta_x = convolve(x, d_x, 'same')
+	delta_y = convolve(x, d_y, 'same')
+	return np.hstack([delta_x.flatten(), delta_y.flatten()])
 
 def MatVec(x):
 	# ---------
 	# your code
 	# ---------
-	pass
+	y = x.reshape(imgShape)
+	return np.hstack([C(y), rho*D(y)])
 	
 def RMatVec(x):
 	# ---------
 	# your code
 	# ---------
-	pass
+	y1 = x[:pix].reshape(imgShape)
+	y2 = x[pix:2*pix].reshape(imgShape)
+	y3 = x[2*pix:3*pix].reshape(imgShape)
+
+	C_t = correlate(y1, kernel, 'same').flatten()
+	delta_x_t = correlate(y2, rho*d_x, 'same').flatten()
+	delta_y_t = correlate(y3, rho*d_y, 'same').flatten()
+
+	return C_t + delta_x_t + delta_y_t
 
 
 def shrink(a, k):
@@ -91,8 +117,16 @@ def EvaluateLinOp():
 	# ---------
 	# your code
 	# ---------
-	pass
+
+	f = np.ones(pix)
+	h = np.ones(3*pix)
+	rhs1 = MatVec(f)
+	rhs2 = RMatVec(h)
 	
+	val1 = rhs1.dot(h)
+	val2 = rhs2.dot(f)
+	print("val1 = {}\nval2 = {}".format(val1, val2))
+	assert (val1-val2 <= 1.e-5)
 	
 def EvaluateShrink():
 	# ---------
@@ -109,7 +143,7 @@ def EvaluateShrink():
 EvaluateLinOp()
 EvaluateShrink()
 
-# deblurring
+# # deblurring
 plt.imsave("original.png", img)
 blurred = convolve(img, C, 'same')
 blurred += np.random.normal(0, sigma, blurred.shape)
